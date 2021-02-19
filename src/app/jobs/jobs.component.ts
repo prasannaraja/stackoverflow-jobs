f987a7e237e25bc48bc918318d51b838c0ab3a16
Prasanna Raja <prasannaraja@msn.com>
2021-02-19 21:17:59 +0530

    Update jobs.component.ts

--- src/app/jobs/jobs.component.ts
+++ src/app/jobs/jobs.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
-import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { fromFetch } from 'rxjs/fetch';
 
@@ -11,47 +11,25 @@ import { fromFetch } from 'rxjs/fetch';
 })
 export class JobsComponent implements OnInit {
 
-  private location = 'sydney';
-  private stackOverFlowJobsRssFeedUrl = `http://careers.stackoverflow.com/jobs/feed?location=${this.location}`;
-  constructor(private http: HttpClient) { }
+  private proxyPrefix = '/proxy';
+  private location='sydney'
+  private query = `location=${this.location}`;
+  private path = 'jobs/feed';
 
-  private getData$(url: string): Observable<any> {
-    return this.http.get<any>(url);
-  }
+  constructor(private http: HttpClient) { }
     
   private get stackOverflowJobs$(): Observable<any> {
-    return fromFetch(this.stackOverFlowJobsRssFeedUrl, {
+    const proxyURL = `${this.proxyPrefix}/${this.path}?${this.query}`;
+    return fromFetch(proxyURL, {
       selector: response => response.json()
-    });
+    });   
   }
-
-  private get gitHubJobs$(): Observable<any> {
-    return fromFetch('https://jobs.github.com/positions.json?page=1', {
-      selector: response => response.json()
-    });
-  }
-  
+    
   ngOnInit(): void {
 
-    // basic http get request
-    this.getData$(this.stackOverFlowJobsRssFeedUrl)
-      .subscribe((x) => {
-        console.log(x);
-      });
-
-    // rxjs fetch method - githubjobs
-    this.gitHubJobs$.subscribe({
-      next: result => console.log('github jobs:',result),
-      complete: () => console.log('github done')
+    this.stackOverflowJobs$.subscribe({
+      next: result => console.log('jobs:',result),
+      complete: () => console.log('done')
     });
-
-    // using proxy
-    const fetchUsingProxy$ = fromFetch(`/api/jobs/feed?location=${this.location}`, {
-      selector: response => response.json()
-    });
-    
-    fetchUsingProxy$.subscribe((x) => {
-      console.log(x);
-    })
   }
 }
