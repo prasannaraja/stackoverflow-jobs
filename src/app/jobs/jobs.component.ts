bd88275bee1733921fb33f90d0139420e32e3bf7
Prasanna Raja <prasannaraja@msn.com>
2021-02-20 01:34:08 +0530

    added job component

--- src/app/jobs/jobs.component.ts
+++ src/app/jobs/jobs.component.ts
@@ -1,33 +1,56 @@
+import * as xml2js from 'xml2js';
+
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable, bindNodeCallback } from 'rxjs';
-import { catchError, map, switchMap } from 'rxjs/operators';
 
-import { fromFetch } from 'rxjs/fetch';
+import { Observable } from 'rxjs';
+import { StackOverFlowJobs } from 'src/models/rss';
+import { parse } from 'path';
 
 @Component({
   selector: 'app-jobs',
   templateUrl: './jobs.component.html',
-  styleUrls: ['./jobs.component.scss']
+  styleUrls: ['./jobs.component.scss'],
 })
 export class JobsComponent implements OnInit {
-
   private proxyPrefix = '/proxy';
-  private location='sydney'
+  private location = 'sydney';
   private query = `location=${this.location}`;
   private path = 'jobs/feed';
 
-  constructor(private http: HttpClient) { }
-    
-  private get stackOverflowJobs$(): Observable<any> {
-    const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json', }), responseType: 'text' as 'json' };
-    const proxyURL = `${this.proxyPrefix}/${this.path}?${this.query}`;
-    return this.http.get<any>(proxyURL, httpOptions);
-  }
-   
-  ngOnInit(): void {
+  constructor(private http: HttpClient) {}
 
-    this.stackOverflowJobs$
-      .subscribe(console.log);
+  private get sydneyJobsUrl(): string {
+    return `${this.proxyPrefix}/${this.path}?${this.query}`;
+  }
+
+  private get allJobsUrl(): string {
+    return `${this.proxyPrefix}/${this.path}`;
+  }
+
+  private get stackOverflowJobs$(): Observable<any> {
+    const httpOptions = {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+      responseType: 'text' as 'json',
+    };
+    return this.http.get<any>(this.allJobsUrl, httpOptions);
+  }
+
+  ngOnInit(): void {
+    this.stackOverflowJobs$.subscribe(
+      (data) => {
+        const p: xml2js.Parser = new xml2js.Parser();
+        p.parseString(data, (err: any, result: any) => {
+          if (err) {
+            throw err;
+          }
+          const jobs: StackOverFlowJobs = result as StackOverFlowJobs;
+          console.log(jobs.rss);
+        });
+      },
+      (error) => {
+        console.log(error, 'err');
+      }
+    );
   }
 }
